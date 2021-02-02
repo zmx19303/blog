48ba293f82eb0a9f46de7b95855091c8fd437196
chaomeng <zmx19303@126.com>
2021-02-02 23:46:50 +0800

    新增idea方法和类注释模板 博客

--- src/com/personal/chaomeng/HelloWorld.java
+++ src/com/personal/chaomeng/HelloWorld.java
@@ -11,4 +11,5 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
     }
+
 }
---
+++ src/com/personal/chaomeng/tool/Test.java
@@ -0,0 +1,45 @@
+package com.personal.chaomeng.tool;
+
+/**
+ * <p>类模板</p>
+ *
+ * @author chaomeng
+ * @version V1.0.0
+ * @date 2021-02-02 22:39
+ */
+public class Test {
+
+    /**
+     * <p>方法模板</p>
+     *
+     * @param i TODO
+     * @return {@link String} TODO
+     * @author chaomeng
+     * @date 2021-02-02 23:33
+     */
+    public String get(int i) {
+        return null;
+    }
+
+    /**
+     * <p>TODO</p>
+     *
+     * @return {@link String} TODO
+     * @author chaomeng
+     * @date 2021-02-02 23:34
+     */
+    public String get() {
+        return null;
+    }
+
+    /**
+     * <p>TODO</p>
+     *
+     * @param str TODO
+     * @author chaomeng
+     * @date 2021-02-02 23:34
+     */
+    public void set(String str) {
+    }
+    
+}
