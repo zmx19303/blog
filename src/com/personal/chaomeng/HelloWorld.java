98344001edd6d42396598da7cf6e1907b257b1ed
zmx19303 <zmx19303@126.com>
2021-02-02 21:18:47 +0800

    init dir

--- src/com/personal/chaomeng/HelloWorld.java
+++ src/com/personal/chaomeng/HelloWorld.java
@@ -7,7 +7,7 @@ package com.personal.chaomeng;
  * @version V1.0.0
  * @date 2021-2-2 21:17
  */
-public class Helloworld {
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
     }
